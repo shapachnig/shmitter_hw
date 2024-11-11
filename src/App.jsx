8c6a7e374ee24e58c3d0c4d70c213db34ad255f1
edward <edwardlfish@gmail.com>
2024-11-11 18:16:25 +0200

    fix changeAvatar if url empty string or null

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     });
 
     const changeAvatar = url => {
-        setUser({...user, avatar: url});
+        setUser({...user, avatar: url || user.avatar});
     }
 
     return (
